0dd8355871fd150d966d8b0bb766a032a536c4c6
Nisma <20-41982-1@student.aiub.edu>
2023-10-19 23:45:32 +0600

    did the initail setup taildwind,daisyui,react router

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className='text-4xl font-semibold'>Entertainment and Media</h1>
+    
+    </>
+  )
+}
+
+export default App
